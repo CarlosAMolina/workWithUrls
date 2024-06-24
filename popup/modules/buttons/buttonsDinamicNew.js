779a58e5a14c2f162cdfb35c6e2815f12bdb1d6b
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-06-24 19:51:59 +0200

    Partial commit add new files

---
+++ popup/modules/buttons/buttonsDinamicNew.js
@@ -0,0 +1,89 @@
+import * as ModuleDom from "../../../popup/modules/dom.js";
+/* TODO replace buttonsDinamic whit this file when it's ready */
+
+class ButtonInterface {
+  constructor(config) {
+    this._configDefault = {
+      tag: "button",
+      attributes: {
+        title: undefined,
+        class: "buttonNew squareButton",
+      },
+      style: {
+        width: "30px",
+        height: "30px",
+        background: undefined,
+      },
+    };
+    this._config = this._updateConfig(config);
+  }
+
+  get button() {
+    return new ModuleDom.DocumentModifier().createElement(this._config);
+  }
+
+  _updateConfig(config) {
+    let result = this._configDefault;
+    result.attributes.title = config.attributes.title;
+    result.style.background = `url('${config.style.backgroundUrlSrc}') no-repeat center`;
+    return result;
+  }
+}
+
+class ButtonCancel extends ButtonInterface {
+  constructor() {
+    const config = {
+      attributes: {
+        title: "Cancel update",
+      },
+      style: {
+        backgroundUrlSrc: "/icons/cancel.png",
+      },
+    };
+    super(config);
+  }
+}
+
+class ButtonDelete extends ButtonInterface {
+  constructor() {
+    const config = {
+      attributes: {
+        title: "Delete",
+      },
+      style: {
+        backgroundUrlSrc: "/icons/trash.png",
+      },
+    };
+    super(config);
+  }
+}
+
+class ButtonEdit extends ButtonInterface {
+  constructor() {
+    const config = {
+      attributes: {
+        title: "Edit",
+      },
+      style: {
+        backgroundUrlSrc: "/icons/edit.png",
+      },
+    };
+    super(config);
+  }
+}
+
+class ButtonUpdate extends ButtonInterface {
+  constructor() {
+    const config = {
+      attributes: {
+        title: "Update",
+      },
+      style: {
+        backgroundUrlSrc: "/icons/ok.png",
+      },
+    };
+    super(config);
+  }
+}
+
+export { ButtonCancel, ButtonDelete, ButtonEdit, ButtonUpdate };
---
+++ popup/modules/buttons/buttonsFactoryNew.js
@@ -0,0 +1,67 @@
+/* TODO  replace buttonsFactory with buttonsFactoryNew, rename drop New */
+import * as ModuleButtonAddRule from "./buttonAddRule.js";
+import * as ModuleButtonCleanUrl from "./buttonCleanUrl.js";
+import * as ModuleButtonClearAllRules from "./buttonClearAllRules.js";
+import * as ModuleButtonConfiguration from "./buttonConfiguration.js";
+import * as ModuleButtonConfigurationRules from "./buttonConfigurationRules.js";
+import * as ModuleButtonCopy from "./buttonCopy.js";
+import * as ModuleButtonObfuscate from "./buttonObfuscate.js";
+import * as ModuleButtonOpenUrls from "./buttonOpenUrls.js";
+import * as ModuleButtonsDinamic from "./buttonsDinamicNew.js";
+import * as ModuleButtonsExceptions from "./buttonsExceptions.js";
+import * as ModuleButtonsLazyLoading from "./buttonsLazyLoading.js";
+import * as ModuleButtonsOnOff from "./buttonsOnOff.js";
+import * as ModuleButtonsRulesType from "./buttonsRulesType.js";
+
+/*
+param buttonId: str.
+*/
+function getButton(buttonId) {
+  //console.log(`Button ID: ${buttonId}`) //Only for development
+  switch (buttonId) {
+    case "cancel":
+      return new ModuleButtonsDinamic.ButtonCancel().button;
+    case "delete":
+      return new ModuleButtonsDinamic.ButtonDelete().button;
+    case "edit":
+      return new ModuleButtonsDinamic.ButtonEdit().button;
+    case "update":
+      return new ModuleButtonsDinamic.ButtonUpdate().button;
+    case ModuleButtonAddRule.ButtonAddRule.buttonIdHtml:
+      return new ModuleButtonAddRule.ButtonAddRule();
+    case ModuleButtonCleanUrl.ButtonCleanUrl.buttonIdHtml:
+      return new ModuleButtonCleanUrl.ButtonCleanUrl();
+    case ModuleButtonClearAllRules.ButtonClearAllRules.buttonIdHtml:
+      return new ModuleButtonClearAllRules.ButtonClearAllRules();
+    case ModuleButtonCopy.ButtonCopy.buttonIdHtml:
+      return new ModuleButtonCopy.ButtonCopy();
+    case ModuleButtonConfiguration.ButtonConfiguration.buttonIdHtml:
+      return new ModuleButtonConfiguration.ButtonConfiguration();
+    case ModuleButtonConfigurationRules.ButtonConfigurationRules.buttonIdHtml:
+      return new ModuleButtonConfigurationRules.ButtonConfigurationRules();
+    case ModuleButtonObfuscate.ButtonObfuscate.buttonIdHtml:
+      return new ModuleButtonObfuscate.ButtonObfuscate();
+    case ModuleButtonOpenUrls.ButtonOpenUrls.buttonIdHtml:
+      return new ModuleButtonOpenUrls.ButtonOpenUrls();
+    case ModuleButtonsLazyLoading.ButtonAddLazyLoading.buttonIdHtml:
+      return new ModuleButtonsLazyLoading.ButtonAddLazyLoading();
+    case ModuleButtonsLazyLoading.ButtonConfigurationLazyLoading.buttonIdHtml:
+      return new ModuleButtonsLazyLoading.ButtonConfigurationLazyLoading();
+    case ModuleButtonsOnOff.ButtonDecodeUrls.buttonIdHtml:
+      return new ModuleButtonsOnOff.ButtonDecodeUrls();
+    case ModuleButtonsOnOff.ButtonOpenPaths.buttonIdHtml:
+      return new ModuleButtonsOnOff.ButtonOpenPaths();
+    case ModuleButtonsOnOff.ButtonOpenRules.buttonIdHtml:
+      return new ModuleButtonsOnOff.ButtonOpenRules();
+    case ModuleButtonsRulesType.ButtonInputDeobfuscation.buttonIdHtml:
+      return new ModuleButtonsRulesType.ButtonInputDeobfuscation();
+    case ModuleButtonsRulesType.ButtonInputObfuscation.buttonIdHtml:
+      return new ModuleButtonsRulesType.ButtonInputObfuscation();
+    default:
+      throw new ModuleButtonsExceptions.ButtonNameInvalidException(
+        `Invalid buttonId: ${buttonId}`,
+      );
+  }
+}
+
+export { getButton };
