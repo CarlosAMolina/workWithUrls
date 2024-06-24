83f1f36802505a212a7781c6f260200738888e63
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-06-24 07:15:50 +0200

    Add function to replace old one

--- test/menuStoredRule.js
+++ test/menuStoredRule.js
@@ -26,17 +26,23 @@ function storageMockGet() {
 
 describe("Check script menuStoredRule.js: ", function () {
   describe("Check function showMenuStoredRule: ", function () {
-    // TODO incorrect test.
-    // TODO drop only
-    it.only("Check function runs without exceptions: ", function () {
+    it("Check function runs without exceptions: ", function () {
       const rule = new ModuleRule.Rule("http", "hXXp");
       let rules = new ModuleUrlsModifier.Rules();
       ModuleMenuStoredRule.showMenuStoredRule(rule, rules);
       const result = ModuleDom.getInfoContainer().innerHTML;
       const expectedResult =
         '<div><div><button title="Delete" class="floatLeft button" style="width: 30px; height: 30px; background: url(/icons/trash.png) no-repeat center;"></button><button title="Edit" class="floatLeft button" style="width: 30px; height: 30px; background: url(/icons/edit.png) no-repeat center;"></button><p style="margin-left: 75px">http ---&gt; hXXp</p><div class="clearfix"></div></div><div style="display: none;"><input class="input" style="width:30%"><input class="input" style="width:30%"><button title="Update" class="floatLeft button" style="width: 30px; height: 30px; background: url(/icons/ok.png) no-repeat center;"></button><button title="Cancel update" class="floatLeft button" style="width: 30px; height: 30px; background: url(/icons/cancel.png) no-repeat center;"></button><div class="clearfix"></div></div></div>';
-      // TODO const expectedNewResult = '<div class="section configRule"><button title="Delete" class="buttonNew squareButton"><img src="/icons/trash.png" /></button><button title="Edit" class="buttonNew squareButton"><img src="/icons/edit.png" /></button><p>old value ---&gt; new value</p>';
       chai.expect(expectedResult).to.equal(result);
+    });
+    // TODO drop only
+    it.only("Check function runs without exceptions: ", function () {
+      const rule = new ModuleRule.Rule("http", "hXXp");
+      let rules = new ModuleUrlsModifier.Rules();
+      ModuleMenuStoredRule.showMenuStoredRuleNew(rule, rules);
+      const newResult = ModuleDom.getInfoContainer().innerHTML;
+      const expectedNewResult = '<div class="section configRule"><button title="Delete" class="buttonNew squareButton"><img src="/icons/trash.png" /></button><button title="Edit" class="buttonNew squareButton"><img src="/icons/edit.png" /></button><p>old value ---&gt; new value</p>';
+      chai.expect(expectedNewResult).to.equal(newResult);
     });
     describe("Check function updateValue: ", function () {
       it("Check function runs without exceptions: ", function () {
