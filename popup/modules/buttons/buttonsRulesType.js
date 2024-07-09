cfaaad125656e531032c057133e784a16d674aff
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-07-09 19:45:35 +0200

    Drop unrequired element id

--- popup/modules/buttons/buttonsRulesType.js
+++ popup/modules/buttons/buttonsRulesType.js
@@ -5,7 +5,6 @@ import * as ModuleMenuStoredRule from "../../modules/menus/menuStoredRule.js";
 import * as ModuleUrlsModifier from "../../modules/urlsModifier.js";
 
 const _elementsIdToEnable = [
-  "pInputNew",
   "inputValueOld",
   "inputValueNew",
   "inputRules",
