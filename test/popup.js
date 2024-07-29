84e989c0beee89a86878d2e60e54fc38bb565c62
CarlosAMolina <15368012+CarlosAMolina@users.noreply.github.com>
2024-07-29 21:39:13 +0200

    fix eslint error

--- test/popup.js
+++ test/popup.js
@@ -1,3 +1,7 @@
+/* https://eslint.org/docs/latest/use/configure/language-options#specifying-globals */
+/* global browser */
+/* global global */
+
 import { describe } from "mocha";
 import { it } from "mocha";
 import chai from "chai";
